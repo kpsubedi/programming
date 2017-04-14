1c5297194eec971ea77b159cb4689ff09a5cab6a
Kul <kul.subedi@gmail.com>
2017-04-14 10:21:17 -0500

    started

---
+++ linux_system_programming/file_copy_low_level_syscall.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#define BUFFSIZE 16384
+
+void main()
+{
+    int fdin, fdout; /* INPUT and OUTPUT descriptors */
+    char buff[BUFFSIZE];
+    int count;
+
+    if ((fdin = open("comp3410", O_RDONLY)) < 0) {
+	perror("comp3410");
+	exit(1);
+    }
+    if ((fdout = open("comp3410_copy", O_WRONLY | O_CREAT, 0644)) < 0 ) {
+	perror("comp3410_copy");
+	exit(2);
+    }
+
+    while ((count = read(fdin, buff, BSIZE)) > 0)
+	write(fdout, buff, count);
+
+    close(fdin);
+    close(fdout);
+}
+
