40d32a2d436f7d58d4c0b8d5d31c77bcc13beb91
Kul Subedi <subedi.kulprasad@gmail.com>
2017-12-26 09:47:22 -0600

    Update code.py

--- python_programming/working_with_algp/code.py
+++ python_programming/working_with_algp/code.py
@@ -37,6 +37,53 @@ def performance():
 
 performance()
 #######################################
+# Binary Search template file
+#######################################
+from time import time
+
+def contains(collection, target):
+    """" Determine whether collection contains target."""
+    return target in collection
+
+def bs_contains(ordered, target):
+    """ Use binary array search to return index position of target in collection"""
+
+    low = 0
+    high = len(ordered) - 1
+    while low <= high:
+        mid = (low+high)/2
+        if target == ordered[mid]:
+            return mid
+        elif target < ordered[mid]:
+            high = mid - 1
+        else:
+            low = mid + 1
+
+    return -(low + 1)
+
+def insert_in_place(ordered, target):
+    """Inserts target into it proper location"""
+    idx = bs_contains(ordered, target)
+    if idx < 0:
+        ordered.insert(-(idx+1), target)
+        return
+    ordered.append(idx, target)
+
+def performance():
+    """ Demonstrate execution performance of contains"""
+    n = 1024
+    while n < 50000000:
+        sorted = range(n)
+        now = time()
+        # Code whose performance is to be evaluated
+        #bs_contains(sorted, -1)
+        insert_in_place(sorted, n+1)
+        done = time()
+        print(n, (done-now)*1000)
+        n = n * 2
+
+performance()
+#######################################
 ####Binary Search Tree#################
 # Binary Search Tree Implementation
 class BinaryNode:
