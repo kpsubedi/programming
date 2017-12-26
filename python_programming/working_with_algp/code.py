606a4c25a33d205e2e29d6897617a38b9b2a2cd1
Kul Subedi <subedi.kulprasad@gmail.com>
2017-12-26 11:20:26 -0600

    Update code.py

--- python_programming/working_with_algp/code.py
+++ python_programming/working_with_algp/code.py
@@ -150,3 +150,108 @@ def performance():
         
         
         
+####Binary Search Tree with remove node feature####
+###################################################
+# Binary Search Tree Implementation
+class BinaryNode:
+
+    def __init__(self, value = None):
+        """Create binary node"""
+        self.value = value
+        self.left = None
+        self.right = None
+    
+    def add(self, val):
+        """Adds a new node to the tree containing this value"""
+        if val <= self.value:
+            if self.left:
+                self.left.add(val)
+            else:
+                self.left = BinaryNode(val)
+        else:
+            if self.right:
+                self.right.add(val)
+            else:
+                self.right = BinaryNode(val)
+    def delete(self):
+        """
+        Remove value of self from BinaryTree. Works in conjunction
+        with remove method in BinaryTree.
+        """
+        if self.left == self.right == None:
+            return None
+        if self.left == None:
+            return self.right
+        if self.right == None:
+            return self.left
+        child = self.left
+        grandchild = child.right
+        if grandchild:
+            while grandchild.right:
+                child = grandchild
+                grandchild = child.right
+            self.value = grandchild.value
+            child.right = grandchild.left
+        else:
+            self.left = child.left
+            self.value = child.value
+        return self    
+
+class BinaryTree:
+    
+    def __init__(self):
+        "Create empty binary tree"""
+        self.root = None
+
+    def add(self, value):
+        """Insert value into proper location in Binary tree"""
+        if self.root is None:
+            self.root = BinaryNode(value)
+        else:
+            self.root.add(value)        
+    
+    def contains(self, target):
+        """Check whether BST contains target value"""
+        node = self.root
+        while node:
+            if target == node.value:
+                return True
+            if target < node.value:
+                node = node.left
+            else:
+                node = node.right
+        return False
+
+    def remove(self, value):
+        """Remove value from tree."""
+        if self.root:
+            self.root = self.remove_from_parent(self.root, value)
+            
+    def remove_from_parent(self, parent, value):
+        """Remove value from tree rooted at parent"""
+        if parent is None:
+            return None
+        if value == parent.value:
+            return parent.delete()
+        elif value < parent.value:
+            parent.left = self.remove_from_parent(parent.left, value)
+        else:
+            parent.right = self.remove_from_parent(parent.right, value)
+
+        return parent    
+
+import random
+from time import time
+
+def performance():
+    """Demonstrate execution performance"""
+    n = 1024
+    while n <= 65536:
+        bt = BinaryTree()
+        for i in range(n):
+            bt.add(random.randint(1,n))
+        now = time()
+        bt.contains(random.randint(1,n))
+        print ("%d, %f" % (n, (time() - now)*1000))
+        n = n * 2
+ #######################################################
