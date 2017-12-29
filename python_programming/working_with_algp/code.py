93f733cf85fe6ff5540152540c7ce946eb91b710
Kul Subedi <subedi.kulprasad@gmail.com>
2017-12-28 19:20:32 -0600

    Update code.py

--- python_programming/working_with_algp/code.py
+++ python_programming/working_with_algp/code.py
@@ -313,3 +313,62 @@ def get_performance_sum():
         print("%d\t%f" %(2**i,scores[i]))
     
 ##############################
+# Divide and Conquer Algo
+##############################
+# MergeSort Implementation
+
+def copy_merge_sort(A):
+    """MergeSort of A and return a new collection"""
+    if len(A) < 2:
+        return A
+    mid = len(A)//2
+    left = copy_merge_sort(A[:mid])
+    right = copy_merge_sort(A[mid:])
+    
+    i = j = 0
+    B = []
+
+    while len(B) < len(A):
+        if j >= len(right) or (i < mid and left[i] < right[j]):
+            B.append(left[i])
+            i = i + 1
+        elif j < len(right):
+            B.append(right[j])
+            j = j + 1
+     
+
+    return B   
+
+
+def merge_sort(A):
+    """Merge Sort A in Place"""
+    copy = list(A)
+    merge_sort_array(copy, A, 0, len(A))
+
+
+def merge_sort_array(A, result, start, end):
+    """Mergesort array in memory with given range"""
+    if end - start < 2:
+        return
+    if end - start == 2:
+        if result[start] > result[start+1]:
+            result[start], result[start+1] = result[start+1], result[start]
+        return
+
+    mid = (end + start)//2
+    merge_sort_array(result, A, start, mid)
+    merge_sort_array(result, A, mid, end)
+
+    # merge is now ready. Merge from A back into result
+    i = start
+    j = mid
+    idx = start
+    while idx < end:
+        if j >= end or (i < mid and A[i] < A[j]):
+            result[idx] = A[i]
+            i = i + 1
+        else:
+           result[idx] = A[j]
+           j = j + 1
+        idx = idx + 1
+   ###
